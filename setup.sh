a5ef17a8eaaf08bf00bf2fed712315c1eb57f462
Federico Parroni <Keyblade95@live.it>
2022-04-16 19:33:49 +0200

    Added gh cli

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 sudo apt update && \
-sudo apt upgrade -y && \
+sudo apt upgrade -y
+
+# install github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) \
+signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] \
+https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt install -y gh
+# install docker
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
