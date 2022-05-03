948f143877aaa521b64b537e842133db4de70178
Federico Parroni <Keyblade95@live.it>
2022-05-03 13:19:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) \
 signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] \
 https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update && \
 sudo apt install -y gh
 # install docker
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && \
